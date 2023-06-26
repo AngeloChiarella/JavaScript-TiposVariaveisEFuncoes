30f16034a755aa86c52a8e6360b1fef6ded16afb
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-26 10:31:30 -0300

    Tipos primitivos - string

---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá mundo!";
+const texto2 = "Olá mundo!";
+const senha = "senhaSuperSegura456!";
+// mesmo sendo numero, se estiver entre "" é uma string
+const StringDeNumeros = "34567";
+
+// '' e "" - Nao tem prioridade
+const citacao1 = ' O Leo disse "Oi"';
+const citacao2 = " O Leo disse 'Oi'";
+// console.log(citacao1, citacao2)
+
+
+//concatenacao
+const citacao3 = "Meu nome e "
+const meuNome = "Angelo";
+console.log(citacao3 + meuNome);
